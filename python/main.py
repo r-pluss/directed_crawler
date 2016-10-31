f420ed010453ed513714dda4201c21d8b6ebde2a
r-pluss <r.pluss52@gmail.com>
2016-10-31 13:26:07 -0400

    Update main.py

--- python/main.py
+++ python/main.py
@@ -2,26 +2,45 @@
 
 import requests
 import simplejson as json
+#as there is no good way to declare anonymous functions, nor to compile new functions
+#dynamicallly in python, we will import a module that will act as our instance-specific
+#config object, as the necessary functions that are unique to a given Directed_Crawler
+#instance can be composed using expected method names. E.g: 
+#   Directed_Crawler_Instance.increment = crawler_config.increment
+#   where increment is simply a function defined in the top-level namespace of the 
+#   crawler_config module
+#if a better method can be devised in the future, it should replace this methodology
+import crawler_config
 
 class Directed_Crawler():
     def __init__(self, init_obj):
         self.last_result = None
         self.previous_increment = None
-        self.response_test = self._make_response_test(init_obj['response_test'])
-        self.template = init_obj['template']
-        self._make_increment_rules(init_obj['increment_rules'])
+        #self.response_test = self._make_response_test(init_obj['response_test'])
+        #self.template = init_obj['template']
+        #self._make_increment_rules(init_obj['increment_rules'])
         
-    def _make_increment_rules(self, rules):
-        #need to solve problem of treating this similar to anonymous function in javascript
-        #each rule should describe a single atomic change
-        self._increment_rules = []
-        for rule in rules:
-            self._increment_rules.append(lambda x: x
+    def _make_increment_rules(self):
+        self._increment_rules = crawler_config.increment_rules
+        #crawler_config.increment_rules should be a list of functions that are defined in the crawler_config module
+        #these function should take two arguments, the result of the previous function (or the previous increment
+        #value if it is the first function) and a reference to the Directed_Crawler instance
+        #e.g.:
+        #def a():
+        #   passs
+        #def b():
+        #   pass
+        #def c():
+        #   pass
+        #def increment_rules():
+        #   return [a, b, c]
+        
         
     def increment(self):
         _ = self.previous_increment
         for rule in self._increment_rules:
-            _ = rule(_)
+            #we pass self so that the rule can operate upon all declared attributes
+            _ = rule(_, self)
         self.current_increment = _
             
     
