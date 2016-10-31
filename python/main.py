ce1b0f62ac5e547b5ce6e4deb45a64b2bdda3ee2
r-pluss <r.pluss52@gmail.com>
2016-10-31 08:36:51 -0400

    Create main.py
    Just some initial comments to help later.

---
+++ python/main.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+#the following stateful properties must be included:
+#self.template
+#self.last_result
+#self.increment_rule
+#self.response_test
+#self.stop_iteration_test
+#self.previous_increment
