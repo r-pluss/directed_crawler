5f4254058c98c85588671ef41249e0defa7d9962
r-pluss <r.pluss52@gmail.com>
2016-10-31 13:34:56 -0400

    Create crawler_config.py

---
+++ python/configs/crawler_config.py
@@ -0,0 +1,9 @@
+#this is an example of the crawler_config file format that is used as a config
+#file for an instance of the Directed_Crawler class
+#this config file can and should be changed for each project and is only used
+#here as prototyping example
+
+#remember to structure the file so that all functions referenced by expected 
+#methods are declared first, as python does not have hoisting à la javascript
+
+
