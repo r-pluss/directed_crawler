fd760841b4da836699f7c4b4157eda133ee50f3a
r-pluss <r.pluss52@gmail.com>
2016-10-31 19:26:02 -0400

    pep_8_lines
    PEP8ified line lengths

--- python/main.py
+++ python/main.py
@@ -1,13 +1,15 @@
 import requests
 import simplejson as json
-#as there is no good way to declare anonymous functions, nor to compile new functions
-#dynamicallly in python, we will import a module that will act as our instance-specific
-#config object, as the necessary functions that are unique to a given Directed_Crawler
-#instance can be composed using expected method names. E.g: 
+#as there is no good way to declare anonymous functions, nor to compile new
+#functions dynamicallly in python, we will import a module that will act
+#as our instance-specific config object, as the necessary functions that are
+#unique to a given Directed_Crawler
+#instance can be composed using expected method names. E.g:
 #   Directed_Crawler_Instance.increment = crawler_config.increment
-#   where increment is simply a function defined in the top-level namespace of the 
-#   crawler_config module
-#if a better method can be devised in the future, it should replace this methodology
+#   where increment is simply a function defined in the top-level namespace
+#   of the crawler_config module
+#if a better method can be devised in the future, it should replace
+#this methodology
 import crawler_config
 
 class Directed_Crawler():
@@ -17,17 +19,19 @@ class Directed_Crawler():
         self.current_increment = None
         self.template = crawler_config.template()
         self._make_increment_rules()
-        #could easily be inlined without the class method call, but am not 
+        #could easily be inlined without the class method call, but am not
         #currently refactoring incase future requirements necessitate an
         #increase in complexity that would be better encapsulated within
         #a function
         self._make_stop_iteration_tests()
-        
+
     def _make_increment_rules(self):
         self._increment_rules = crawler_config.increment_rules
-        #crawler_config.increment_rules should be a list of functions that are defined in the crawler_config module
-        #these function should take two arguments, the result of the previous function (or the previous increment
-        #value if it is the first function) and a reference to the Directed_Crawler instance
+        #crawler_config.increment_rules should be a list of functions that are
+        #defined in the crawler_config module these function should take two
+        #arguments, the result of the previous function (or the previous
+        #increment value if it is the first function) and
+        #a reference to the Directed_Crawler instance
         #e.g.:
         #def a():
         #   passs
@@ -37,12 +41,12 @@ class Directed_Crawler():
         #   pass
         #def increment_rules():
         #   return [a, b, c]
-        
+
     def _make_stop_iteration_tests(self):
         self._stop_iteration_tests = crawler_config.stop_iteration_tests
-        #see _make_increment_rules for details, except tests should only receive a single argument, a reference 
-        #the instance's self obj
-    
+        #see _make_increment_rules for details, except tests should only
+        #receive a single argument, a reference the instance's self obj
+
     def advance(self):
         for test in self._stop_iteration_tests:
             if test(self):
@@ -50,26 +54,27 @@ class Directed_Crawler():
             else:
                 return False
         return True
-        
+
     def increment(self):
         _ = self.previous_increment
         for rule in self._increment_rules:
-            #we pass self so that the rule can operate upon all declared attributes
+            #we pass self so that the rule can operate upon all
+            #declared attributes
             _ = rule(_, self)
         self.current_increment = _
-        
+
     def fetch_next(self):
         return requests.get(self.current_increment)
-    
+
     def process_resource(self):
-        #this is another method that will vary completely depending on what we want
-        #to do with the results of a valid resource and hence will needed to be
-        #defined almost exclusively in the config file
+        #this is another method that will vary completely depending on what
+        #we want to do with the results of a valid resource and hence will
+        #needed to be defined almost exclusively in the config file
         pass
-    
+
     def validate_resource(self):
         return crawler_config.is_valid_resource(self.last_response)
-    
+
     def start(self):
         if crawler_config.template_is_valid_increment:
             self.current_increment = self.template
