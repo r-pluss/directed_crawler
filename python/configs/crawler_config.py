6c06661d55d0427f49f63b5f1b43f626475deee9
r-pluss <r.pluss52@gmail.com>
2016-11-14 13:48:59 -0500

    Update crawler_config.py
    Added first increment rule
    Updated is_valid_resource()

--- python/configs/crawler_config.py
+++ python/configs/crawler_config.py
@@ -6,23 +6,35 @@
 #remember to structure the file so that all functions referenced by expected 
 #methods are declared first, as python does not have hoisting à la javascript
 
+import bs4
+
+
 #module level properties
 template_is_valid_increment = True
 
 
-
+def inc_id(url, crawler):
+    numerics = []
+    current_position = len(url) - 1
+    while url[current_position].isdigit():
+        numerics.insert(0, url[current_position])
+    return url[0 : current_position + 1] + str(int(''.join(numerics)) + 1)
+        
+    
 def save_content(response):
     #persist the resource to disk
     return response
 
 def increment_rules():
-    return []
+    return [inc_id]
 
 def is_valid_resource(response):
     if not response.ok:
         return False
-    else:
-        return True
+    html = bs4.BeautifulSoup(response.content, 'lxml')
+    if html.find(string = 'An error has occurred - Invalid Page') is not None:
+        return False
+    return True
 
 def process_resource():
     #return a list of methods declared in this config module that be
